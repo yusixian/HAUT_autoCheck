f066f5b5869f3492c15b7f9a59c453179f4be2ff
cos <43498495+yusixian@users.noreply.github.com>
2021-02-01 21:43:12 +0800

    Update campus.py

--- campus/campus.py
+++ campus/campus.py
@@ -42,7 +42,7 @@ class CampusCard:
             'exchangeFlag': True,
             'login': False,
             'serverPublicKey': '',
-            'deviceId': 'ffffffff-8c3b-1289-0000-00000033c587',
+            'deviceId': "fffffff-8c3b-1289-0000-00000033c587",
             'wanxiaoVersion': 10462101,
             'rsaKey': {
                 'private': rsa_keys[1],
